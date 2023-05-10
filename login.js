e886d5ca479256da42382044d33e7082f5201040
Laraib <azmatlaraib2003@gmail.com>
2023-05-10 19:14:00 +0500

    initial commits

---
+++ login.js
@@ -0,0 +1,34 @@
+var count = 2;
+
+function validate ()
+{
+   var user = document.login.username.value;
+   var password = document.login.password.value;
+   var valid = false;
+   var usernameArray = ["Laraib"]
+   var passwordArray = ["123"]
+   for (var i = 0; i < usernameArray.length; i++)
+
+if (user == usernameArray[i])
+if (password == passwordArray [i])
+{
+   valid = true;
+   break;
+}
+
+if(valid)
+{
+   alert("Login was successful");
+   window.location = "wwww.google.ie"
+   return false ;
+}
+var again = "tries";
+if (count ==1)
+{
+   again = "try"
+}
+if(count >= 1)
+{
+   alert("Wrong password or username")
+   count--;
+}}
